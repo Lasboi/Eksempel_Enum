e19b46cf4412b08cd38503a5e1df9f4b13b584a2
Lasse <soxdrengen@gmail.com>
2019-09-30 11:34:39 +0200

    Tilføjet lidt logik til et sten saks papir spil

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +13,24 @@ public class Main {
         System.out.println(runde1.slaa(a, b));
         System.out.println(runde2.slaa(b, a));
         System.out.println(runde3.slaa(a, a));
+
+        Hand computerHand = null;
+        int handInt = (int) (Math.random() * 3 + 1);
+        if (handInt == 1) {computerHand = Hand.PAPIR;}
+        if (handInt == 2) {computerHand = Hand.STEN;}
+        if (handInt == 3) {computerHand = Hand.SAKS;}
+        System.out.println("Computeren slår: " + computerHand);
+
+        Scanner scanner = new Scanner(System.in);
+        Hand spiller = null;
+        String spillerSkriver = scanner.next();
+        if (spillerSkriver.equalsIgnoreCase("papir")) {spiller = Hand.PAPIR;}
+        if (spillerSkriver.equalsIgnoreCase("sten")) {spiller = Hand.STEN;}
+        if (spillerSkriver.equalsIgnoreCase("saks")) {spiller = Hand.SAKS;}
+        System.out.println("Spiller slår: " + spiller);
+
+        int resultat = runde1.slaa(computerHand, spiller);
+
+        System.out.println("Resultatet er: " + resultat);
     }
 }
